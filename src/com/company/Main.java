e010e565bddc81d4f65b39ef979ee7cc23a60f3a
theerlanych <theerlanych@gmail.com>
2022-04-03 16:55:36 +0600

    HMWRK3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,21 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        double[] numbers = {15.4, 13.2, 1.4, -3.4, 5.6, 9.2, 11.1, -4.2, 5.5, 14.1, 11.1, 14.2, 13.3, 15.6};
+        double[] numbers = {16.4, 12.3, 1.4, -3.4, 5.6, 9.2, 11.1, -4.2, 5.5, 14.1, 11.1, 14.2, 13.3, 15.6};
         int numberCount = 0;
         double allNumber = 0.0;
-        boolean proverka = false;
+        boolean check = false;
 
         for (double cast : numbers) {
             if (cast < 0) {
-                proverka = true;
-            } else if (cast > 0 && proverka) {
+                check = true;
+            } else if (cast > 0 && check) {
                 numberCount++;
                 allNumber += cast;
                 System.out.println(cast);
             }
 
         }
+        System.out.println("Среднее арифметичское = " + allNumber / numberCount);
 
 
     }
