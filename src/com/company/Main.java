dd4b85799993360de51b608a46948c410e4e65c6
theerlanych <theerlanych@gmail.com>
2022-04-03 17:55:43 +0600

    HMWRK3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        double[] numbers = {16.4, 12.3, 1.4, -3.4, 5.6, 9.2, 11.1, -4.2, 5.5, 14.1, 11.1, 14.2, 13.3, 15.6};
+        double[] numbers = {15.4, 13.2, 1.4, -3.4, 5.6, 9.2, 11.1, -4.2, 5.5, 14.1, -11.1, 14.2, 13.3, 15.6};
         int numberCount = 0;
         double allNumber = 0.0;
         boolean check = false;
@@ -20,9 +22,26 @@ public class Main {
         }
         System.out.println("Среднее арифметичское = " + allNumber / numberCount);
 
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    double temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(numbers));
+
+        }
 
     }
-
 }
+
+
+
+
+
+
+
 
 
